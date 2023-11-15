57b50b08330d2348e582df4e425b4420dd72b90d
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-14 22:20:27 -0500

    Added: check db for email

--- src/main/java/com/example/rentalmanagerapp/Registration/EmailValidator.java
+++ src/main/java/com/example/rentalmanagerapp/Registration/EmailValidator.java
@@ -1,12 +1,25 @@
 package com.example.rentalmanagerapp.Registration;
 
+import com.example.rentalmanagerapp.User.User;
+import com.example.rentalmanagerapp.User.UserRepository;
+import lombok.AllArgsConstructor;
+
+import java.util.Optional;
+
+@AllArgsConstructor
 public class EmailValidator {
+
+   private final UserRepository userRepository;
+
     // Todo regex for email validation
     public boolean validate(String email){
         return true;
     }
-    public boolean validateAgainstDb(String email){
+
+    public boolean validateAgainstDb(String email) {
         //Todo check db for existing acc with email if none return true
-        return true;
+        Optional<User> isExisting = userRepository.findByEmail(email);
+        return isExisting.isEmpty();
     }
+
 }
