347d3697aa97c66fae0033984dab55acc5ada1f9
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-15 23:51:58 -0500

    Added: update user service method

--- src/main/java/com/example/rentalmanagerapp/User/UserService.java
+++ src/main/java/com/example/rentalmanagerapp/User/UserService.java
@@ -1,4 +1,14 @@
 package com.example.rentalmanagerapp.User;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public String updateUser(User user){
+        if(user == null){
+            throw new IllegalStateException("Cannot update");
+        }
+        return "";
+    }
 }
