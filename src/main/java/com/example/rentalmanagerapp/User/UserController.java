17c424757f3b4d2871c6c8be8f09b7e2f0abc2c2
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-15 23:51:22 -0500

    Added: update user method

--- src/main/java/com/example/rentalmanagerapp/User/UserController.java
+++ src/main/java/com/example/rentalmanagerapp/User/UserController.java
@@ -1,4 +1,20 @@
 package com.example.rentalmanagerapp.User;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@AllArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PutMapping("/update")
+    public String updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
 }
