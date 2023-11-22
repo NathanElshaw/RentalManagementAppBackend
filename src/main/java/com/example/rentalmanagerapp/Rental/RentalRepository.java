c253206c73ed8cae474270feccde73514aa398d8
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-21 21:56:22 -0500

    Added repository for id

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
@@ -1,4 +1,8 @@
 package com.example.rentalmanagerapp.Rental;
 
-public interface RentalRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
 }
