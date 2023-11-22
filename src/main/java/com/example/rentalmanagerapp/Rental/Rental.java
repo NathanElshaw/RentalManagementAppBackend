c2a0f80f418d6ab7337c667a5ecbc7839df0a239
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-21 21:55:42 -0500

    Started model for db

--- src/main/java/com/example/rentalmanagerapp/Rental/Rental.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Rental.java
@@ -1,4 +1,27 @@
 package com.example.rentalmanagerapp.Rental;
 
+import com.example.rentalmanagerapp.Rental.Rentee.Rentee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Entity
+@Table(name="Rental", schema = "")
 public class Rental {
+
+    @Id
+    private Long id;
+    private String rentalAddress;
+    private Rentee renter;
+    private int totalTenants;
+
+    public Rental() {
+
+    }
+
+    @OneToOne
+    @JoinColumn(name="Renter_id", nullable = false)
+    public Rentee getRenter() {
+        return renter;
+    }
 }
---
+++ src/main/java/com/example/rentalmanagerapp/Rental/Rentee/Rentee.java
@@ -0,0 +1,2 @@
+package com.example.rentalmanagerapp.Rental.Rentee;public class Rentee {
+}
