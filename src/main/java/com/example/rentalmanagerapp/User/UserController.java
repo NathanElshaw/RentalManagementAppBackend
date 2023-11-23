00435b53fcd1cf57f0d5fd515c77bd9182bd4de1
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 17:15:01 -0500

    Added: Create, Update and, Start on Delete User methods

--- src/main/java/com/example/rentalmanagerapp/User/UserController.java
+++ src/main/java/com/example/rentalmanagerapp/User/UserController.java
@@ -1,20 +1,29 @@
 package com.example.rentalmanagerapp.User;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/user")
 @AllArgsConstructor
+@RequestMapping("/api/v1/user")
 public class UserController {
 
-    private final UserService userService;
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String createUser(@RequestBody UserSignUpRequest user){
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteUser(@RequestParam("User") String username){
+        //Todo validate jwt , add delete service
+        return "";
+    }
 
     @PutMapping("/update")
     public String updateUser(@RequestBody User user){
+        //Todo validate jwt before updating
         return userService.updateUser(user);
     }
 }
