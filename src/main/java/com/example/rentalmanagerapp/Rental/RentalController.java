8c205a8df5e00142c6e9070ba67cc583086ad038
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 17:11:33 -0500

    Started boiler plate for rentals

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalController.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalController.java
@@ -1,4 +1,26 @@
 package com.example.rentalmanagerapp.Rental;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/rentals")
 public class RentalController {
+
+    @PostMapping("/create")
+    public String createRental(){
+        //Todo check priv, add service, validate jwt
+        return "";
+    }
+
+    @PutMapping("/update")
+    public String updateRental(){
+        //Todo check priv, add service, validate
+        return "";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteRental(){
+        //Todo check priv, add service, validate
+        return "";
+    }
 }
