5f12644ffa440d14db1f05a30ad2d659b40f0362
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 17:13:03 -0500

    Made all route accessible

--- src/main/java/com/example/rentalmanagerapp/Security/Config/WebSecurityConfig.java
+++ src/main/java/com/example/rentalmanagerapp/Security/Config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.rentalmanagerapp.Security.Config;
 
+import com.example.rentalmanagerapp.User.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,10 +21,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @AllArgsConstructor
 public class WebSecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/v*/register/**").permitAll();
+                    auth.requestMatchers("/**","/error", "/api/v*/user/register/**").permitAll();
                 })
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(withDefaults())
