0f019b838042a4e9d94d21af8555f55aaa2ba708
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 22:21:02 -0500

    Added: findByAddress use to repository

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
@@ -3,6 +3,9 @@ package com.example.rentalmanagerapp.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    Optional<Rental>findByRentalAddress(String address);
 }
