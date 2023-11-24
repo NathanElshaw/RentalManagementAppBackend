355eaa420848827dcdb0750ab7159ce4c44b1009
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 22:19:46 -0500

    Added: rental code to assign to rentals

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodes.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodes.java
@@ -1,2 +1,39 @@
-package com.example.rentalmanagerapp.Rental.RentalCodes;public class UnitCodes {
+package com.example.rentalmanagerapp.Rental.RentalCodes;
+
+import com.example.rentalmanagerapp.Rental.Rental;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@AllArgsConstructor
+@Entity
+@Table(name="rentalCodes")
+public class UnitCodes {
+    @SequenceGenerator(
+            name = "rentalCodeSequence",
+            sequenceName = "rentalCodeSequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "rentalCodeSequence"
+    )
+    @Id
+    private Long id;
+    private String unitCode;
+    @OneToOne
+    private Rental rental_id;
+    private LocalDateTime issuedAt;
+    private LocalDateTime expiresAt;
+
+    public UnitCodes(){
+
+    }
+
+
 }
