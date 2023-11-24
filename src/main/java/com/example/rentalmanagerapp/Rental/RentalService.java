3c4782ac458360a892043c7672de613583471a79
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 22:21:43 -0500

    Created: createRental method

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalService.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalService.java
@@ -1,4 +1,23 @@
 package com.example.rentalmanagerapp.Rental;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@AllArgsConstructor
 public class RentalService {
+
+    private final RentalRepository rentalRepository;
+    public String createRental(RentalRequest rentalRequest){
+        Rental newRental = new Rental(
+                rentalRequest.getRentalAddress(),
+                rentalRequest.getDescription(),
+                rentalRequest.getRentAmount(),
+                LocalDate.parse(rentalRequest.getDateAvailable())
+        );
+        rentalRepository.save(newRental);
+        return "";
+    }
 }
