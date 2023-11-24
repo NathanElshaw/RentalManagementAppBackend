ce55e150e14a48bf843c2316632c26aa1bb510dd
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-23 22:19:21 -0500

    Added: more items for model

--- src/main/java/com/example/rentalmanagerapp/Rental/Rental.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Rental.java
@@ -1,8 +1,12 @@
 package com.example.rentalmanagerapp.Rental;
 
 import com.example.rentalmanagerapp.Rental.Rentee.Rentee;
+import com.example.rentalmanagerapp.Rental.Units.Units;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.SEQUENCE;
 
@@ -24,12 +28,29 @@ public class Rental {
     @Id
     private Long id;
     private String rentalAddress;
+    private String description;
     @ManyToOne
-    private Rentee renter;
-    private int totalTenants;
+    private Rentee renter = null;
+    @ManyToMany
+    private List<Units> subUnits = null;
+    private int totalTenants = 0;
+    private long rentAmount;
+    private long totalRentIncome = 0L;
+    private LocalDate dateAvailable;
+
 
     public Rental() {
 
+    }
+
+    public Rental(String rentalAddress,
+                  String description,
+                  long rentAmount,
+                  LocalDate dateAvailable) {
+        this.rentalAddress = rentalAddress;
+        this.description = description;
+        this.rentAmount = rentAmount;
+        this.dateAvailable = dateAvailable;
     }
 
     @OneToOne
