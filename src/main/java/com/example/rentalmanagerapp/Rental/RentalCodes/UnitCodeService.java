69b7e032ded39eb56a5c9b7865b3dbbd86f28dc0
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-24 23:09:28 -0500

    Added: findbyUnit code method and setConfirmedAt method for users joining their units

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeService.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeService.java
@@ -1,2 +1,23 @@
-package com.example.rentalmanagerapp.Rental.RentalCodes;public class UnitCodeService {
+package com.example.rentalmanagerapp.Rental.RentalCodes;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UnitCodeService {
+
+    private final UnitCodeRepository unitCodeRepository;
+
+    public Optional<UnitCodes> findByCode(String code){
+        return unitCodeRepository.findByUnitCode(code);
+    }
+
+    public int setConfirmedAt(String token){
+        return unitCodeRepository.updateConfirmedAt(token, LocalDateTime.now());
+    }
 }
