155521584c9c0e677835b084eeef389fb5f4c644
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-24 23:08:37 -0500

    Added: start of confirmed at method for using unitcodes

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeRepository.java
@@ -1,2 +1,20 @@
-package com.example.rentalmanagerapp.Rental.RentalCodes;public interface UnitCodeRepository {
+package com.example.rentalmanagerapp.Rental.RentalCodes;
+
+import lombok.AllArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Code;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UnitCodeRepository extends JpaRepository<UnitCodes, Long> {
+    Optional<UnitCodes> findByUnitCode(String code);
+
+    //Set query here Select token from UnitCode and update confirmedat with confrimedAt date
+    int updateConfirmedAt(String code,
+                          LocalDateTime confirmedAt);
 }
