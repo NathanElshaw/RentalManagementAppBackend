9ab525e4f8ed8f1653059268d4227f5751f61646
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:16:03 -0500

    Added: createUnit method

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsController.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsController.java
@@ -1,2 +1,20 @@
-package com.example.rentalmanagerapp.Rental.Units;public class UnitsController {
+package com.example.rentalmanagerapp.Rental.Units;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/units")
+public class UnitsController {
+
+    private final UnitsService unitsService;
+
+    @PostMapping("/create")
+    public String createUnit(@RequestBody UnitsRequest createUnitPayload){
+        return unitsService.createUnit(createUnitPayload);
+    }
 }
