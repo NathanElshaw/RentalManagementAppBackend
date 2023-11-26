c734c916f11d8f8e6aa5cd7fb1777a70bba0bf42
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:16:25 -0500

    Added: repository

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsRepository.java
@@ -1,2 +1,13 @@
-package com.example.rentalmanagerapp.Rental.Units;public interface UnitsRepository {
+package com.example.rentalmanagerapp.Rental.Units;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UnitsRepository extends JpaRepository<Units, Long> {
+    @Query(value = "select u from Units u where u.unitAddress = ?2 and u.unitNumber = ?1")
+    Optional<Units> findByUnitAddressAndUnitNumber(int unitNumber, String unitAddress);
 }
