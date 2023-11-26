025f47574da8b915498c5d66294ed34e8a636871
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:15:39 -0500

    Changed: types and added constructor for data insertion

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/Units.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/Units.java
@@ -9,8 +9,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import static jakarta.persistence.GenerationType.SEQUENCE;
 
@@ -18,33 +16,40 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 @AllArgsConstructor
 @Getter
 @Setter
-@Table(name = "subUnits")
+@Table(name = "units")
 public class Units {
         @SequenceGenerator(
-                name = "rentalSequence",
-                sequenceName = "rentalSequence",
+                name = "unitsSequence",
+                sequenceName = "unitsSequence",
                 allocationSize = 1
         )
 
         @GeneratedValue(
                 strategy = SEQUENCE,
-                generator = "rentalSequence"
+                generator = "unitsSequence"
         )
-
         @Id
         private Long id;
-        @OneToOne
-        private Rental parentUnitId = null;
+
         @ManyToOne
-        private User Renters = null;
+        @JoinColumn(
+                nullable = false,
+                name = "parent_unit_id"
+        )
+        private Rental parentUnitId;
         @OneToOne
         private UnitCodes unitCode;
-
-        private Long unitNumber;
+        @OneToOne
+        private User renter;
+        private String unitAddress;
+        private int beds;
+        private double baths;
+        private int unitNumber;
         private Boolean hasPets;
-        private Long rentAmount;
-        private Long rentDue;
-        private Long rentPaid;
+        private double rentAmount;
+        private double rentDue;
+        private double rentPaid;
+        private LocalDate leaseStart;
         private LocalDate rentDueDate;
         private LocalDate leaseEnd;
 
@@ -52,8 +57,25 @@ public class Units {
 
         }
 
-        public Units(Long unitNumber, Long rentAmount) {
+        public Units(int unitNumber,
+                     int beds,
+                     double baths,
+                     String unitAddress,
+                     boolean hasPets,
+                     double rentAmount,
+                     String rentDueDate,
+                     String leaseStart,
+                     String leaseEnd,
+                     Rental parentUnitId) {
                 this.unitNumber = unitNumber;
+                this.beds = beds;
+                this.baths = baths;
+                this.unitAddress = unitAddress;
+                this.hasPets = hasPets;
                 this.rentAmount = rentAmount;
+                this.rentDueDate = LocalDate.parse(rentDueDate);
+                this.leaseStart = LocalDate.parse(leaseStart);
+                this.leaseEnd = LocalDate.parse(leaseEnd);
+                this.parentUnitId = parentUnitId;
         }
 }
