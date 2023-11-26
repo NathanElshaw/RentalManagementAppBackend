6e1b69cc3667f879608648e78808fad38b9a93da
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:18:49 -0500

    Added: Confirmed at method

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeRepository.java
@@ -1,8 +1,12 @@
 package com.example.rentalmanagerapp.Rental.RentalCodes;
 
+import com.example.rentalmanagerapp.Rental.Rental;
+import com.example.rentalmanagerapp.Rental.Units.Units;
 import lombok.AllArgsConstructor;
 import org.aspectj.apache.bcel.classfile.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +18,15 @@ import java.util.Optional;
 public interface UnitCodeRepository extends JpaRepository<UnitCodes, Long> {
     Optional<UnitCodes> findByUnitCode(String code);
 
-    //Set query here Select token from UnitCode and update confirmedat with confrimedAt date
+    @Transactional
+    @Modifying
+    @Query("UPDATE UnitCodes c " +
+            "set c.confirmedAt = ?2 "
+            + "WHERE c.unitCode = ?1 ")
     int updateConfirmedAt(String code,
                           LocalDateTime confirmedAt);
+
+    @Query("select rental_id from UnitCodes where unitCode = ?1")
+    Long returnUnitWithCode(String code);
+
 }
