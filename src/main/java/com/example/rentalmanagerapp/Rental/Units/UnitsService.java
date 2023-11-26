609c095febaf3bd765b5b098843146b3e1db9640
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:17:18 -0500

    Added: createUnit method

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsService.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsService.java
@@ -1,2 +1,44 @@
-package com.example.rentalmanagerapp.Rental.Units;public class UnitsService {
+package com.example.rentalmanagerapp.Rental.Units;
+
+import com.example.rentalmanagerapp.Rental.Rental;
+import com.example.rentalmanagerapp.Rental.RentalRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UnitsService {
+
+    private final UnitsRepository unitsRepository;
+    private final RentalRepository rentalRepository;
+
+    public String createUnit(UnitsRequest createUnitPayload){
+        boolean doesExist = unitsRepository.findByUnitAddressAndUnitNumber(createUnitPayload.getUnitNumber(),
+                createUnitPayload.getUnitAddress()).isPresent();
+
+        if(doesExist){
+            throw new IllegalStateException("Unit Already Exists");
+        }
+
+        Rental parentRental = rentalRepository.findByRentalAddress(createUnitPayload.getUnitAddress()).orElseThrow(()->
+                new IllegalStateException("Parent Rental doesnt exist"));
+
+        Units newUnit = new Units(
+                createUnitPayload.getUnitNumber(),
+                createUnitPayload.getBeds(),
+                createUnitPayload.getBaths(),
+                createUnitPayload.getUnitAddress(),
+                createUnitPayload.isHasPets(),
+                createUnitPayload.getRentAmount(),
+                createUnitPayload.getRentDueDate(),
+                createUnitPayload.getLeaseStart(),
+                createUnitPayload.getLeaseEnd(),
+                parentRental
+        );
+
+        unitsRepository.save(newUnit);
+
+        return "Success";
+    }
+
 }
