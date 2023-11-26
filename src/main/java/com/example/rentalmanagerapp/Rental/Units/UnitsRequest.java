6daf57a5ef100fd95a8f51a62846218ca8ae20ea
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:16:44 -0500

    Added: request model

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsRequest.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsRequest.java
@@ -1,2 +1,22 @@
-package com.example.rentalmanagerapp.Rental.Units;public class UnitsRequest {
+package com.example.rentalmanagerapp.Rental.Units;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@ToString
+public class UnitsRequest {
+    private int unitNumber;
+    private int beds;
+    private double baths;
+    private String unitAddress;
+    private boolean hasPets;
+    private double rentAmount;
+    private String rentDueDate;
+    private String leaseStart;
+    private String leaseEnd;
 }
