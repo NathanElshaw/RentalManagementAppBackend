2d75d60487686fe5c09d5a7ae1f0c1e6e4e8bd96
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-25 22:14:54 -0500

    Added: readOnly transactional

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
@@ -1,11 +1,16 @@
 package com.example.rentalmanagerapp.Rental;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     Optional<Rental>findByRentalAddress(String address);
+    
 }
