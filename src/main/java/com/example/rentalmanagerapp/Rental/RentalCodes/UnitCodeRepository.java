fe241ca4298c134b6092bc72ede9331e458a115c
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-26 19:23:29 -0500

    Added: getUnitwithId method and converted update method to void

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodeRepository.java
@@ -1,9 +1,5 @@
 package com.example.rentalmanagerapp.Rental.RentalCodes;
 
-import com.example.rentalmanagerapp.Rental.Rental;
-import com.example.rentalmanagerapp.Rental.Units.Units;
-import lombok.AllArgsConstructor;
-import org.aspectj.apache.bcel.classfile.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -23,10 +19,10 @@ public interface UnitCodeRepository extends JpaRepository<UnitCodes, Long> {
     @Query("UPDATE UnitCodes c " +
             "set c.confirmedAt = ?2 "
             + "WHERE c.unitCode = ?1 ")
-    int updateConfirmedAt(String code,
+    void updateConfirmedAt(String code,
                           LocalDateTime confirmedAt);
 
-    @Query("select rental_id from UnitCodes where unitCode = ?1")
-    Long returnUnitWithCode(String code);
+    @Query("select parentRental from UnitCodes where unitCode = ?1")
+    Long getUnitWithCode(String code);
 
 }
