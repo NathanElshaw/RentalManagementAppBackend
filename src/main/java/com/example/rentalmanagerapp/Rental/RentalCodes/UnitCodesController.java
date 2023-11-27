f641c85d0c28f3cd9f7eec39995a9966782c7d7d
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-26 19:25:02 -0500

    Added: create, update, delete unit code methods and join unit with code methods

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodesController.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalCodes/UnitCodesController.java
@@ -1,2 +1,36 @@
-package com.example.rentalmanagerapp.Rental.RentalCodes;public class UnitCodesController {
+package com.example.rentalmanagerapp.Rental.RentalCodes;
+
+import com.example.rentalmanagerapp.Rental.RentalCodes.Requests.JoinUnitRequest;
+import com.example.rentalmanagerapp.Rental.RentalCodes.Requests.UnitCodesRequest;
+import com.example.rentalmanagerapp.Rental.RentalCodes.Requests.UpdateCodeRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/unitCodes")
+public class UnitCodesController {
+
+    private final UnitCodesService unitCodesService;
+
+    @PostMapping("/createUnitCode")
+    public String createUnitCodes (@RequestBody UnitCodesRequest unitCodePayload){
+        return unitCodesService.createUnitCode(unitCodePayload);
+    }
+
+    @PatchMapping("/joinUnit")
+    public String joinUnit(@RequestBody JoinUnitRequest joinUnitPayload){
+        return unitCodesService.joinUnit(joinUnitPayload);
+    }
+
+    @PatchMapping("/updateCode")
+    public String resendCode(@RequestBody UpdateCodeRequest updateCodePayload){
+        return unitCodesService.updateCode(updateCodePayload);
+    }
+
+    @DeleteMapping("/removeUnitCode")
+    public String removeUnitCode(@RequestBody Long unitId){
+        return unitCodesService.deleteUnitCode(unitId);
+    }
+
 }
