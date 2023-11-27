061bdbed57903b43f98e31d5f1f3a51a748bf623
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-26 19:32:55 -0500

    Added: getUnitCodeParent, addUnitCodeToRental, and addRenterToUnit methods

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/UnitsRepository.java
@@ -1,13 +1,35 @@
 package com.example.rentalmanagerapp.Rental.Units;
 
+import com.example.rentalmanagerapp.Rental.RentalCodes.UnitCodes;
+import com.example.rentalmanagerapp.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UnitsRepository extends JpaRepository<Units, Long> {
+
+    @Query("select u.unitCode from Units u where u.unitAddress = ?1 and u.unitNumber = ?2 ")
+    Optional<UnitCodes> getUnitCodeParent(String unitAddress, int unitNumber);
+
+    @Transactional
+    @Modifying
+    @Query("update Units u " +
+            "set u.unitCode = ?1 " +
+            "where u.unitAddress = ?2 and u.unitNumber = ?3")
+    void addUnitCodeToRental(UnitCodes unitCodePayload, String unitAddress, int unitNumber);
+
+    @Transactional
+    @Modifying
+    @Query("update Units u " +
+            "set u.renter = ?1 " +
+            "where u.id = ?2 ")
+    void addRenterToUnit(User userId, Long unitId);
+
     @Query(value = "select u from Units u where u.unitAddress = ?2 and u.unitNumber = ?1")
     Optional<Units> findByUnitAddressAndUnitNumber(int unitNumber, String unitAddress);
 }
