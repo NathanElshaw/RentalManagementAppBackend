172eba7d2076cbd4068f92143d801e2961972fcf
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-27 20:41:50 -0500

    Added: request for getting unit

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/Requests/GetUnitRequest.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/Requests/GetUnitRequest.java
@@ -1,2 +1,13 @@
-package com.example.rentalmanagerapp.Rental.Units.Requests;public class GetUnitRequest {
+package com.example.rentalmanagerapp.Rental.Units.Requests;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@ToString
+public class GetUnitRequest {
+    private Long userId;
+    private String requestingUser;
 }
