d2e9f4edf41d05316ccf480290b57de45da6dcb7
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-29 21:48:39 -0500

    Start of sessionController method for api requests

--- src/main/java/com/example/rentalmanagerapp/Sessions/SessionsController.java
+++ src/main/java/com/example/rentalmanagerapp/Sessions/SessionsController.java
@@ -1,2 +1,15 @@
-package com.example.rentalmanagerapp.Sessions;public class SessionsController {
+package com.example.rentalmanagerapp.Sessions;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/session")
+public class SessionsController {
+
+    @PostMapping("/create")
+    public String createSession(){
+        return "";
+    }
 }
