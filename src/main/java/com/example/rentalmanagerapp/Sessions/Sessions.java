b0d3f84a61d1524a3debfdbd274e4ec9ff69da86
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-11-29 21:48:14 -0500

    Added: Start of session class for model and contructors

--- src/main/java/com/example/rentalmanagerapp/Sessions/Sessions.java
+++ src/main/java/com/example/rentalmanagerapp/Sessions/Sessions.java
@@ -1,2 +1,30 @@
-package com.example.rentalmanagerapp.Sessions;public class Sessions {
+package com.example.rentalmanagerapp.Sessions;
+
+import com.example.rentalmanagerapp.Sessions.JWT.JWT;
+import com.example.rentalmanagerapp.User.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+public class Sessions {
+    @Id
+    private long id;
+    @OneToOne
+    private JWT jwt;
+    @OneToOne
+    private User user;
+    private int sessionAmounts;
+    private LocalDate lastSession;
+    private LocalDateTime startOfSession;
+
+    public Sessions () {
+
+    }
+
 }
