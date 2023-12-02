c5675b5de4e0c5d8aea160050eafbbe0a60fbc1a
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-01 22:50:04 -0500

    Added: getAllUnits method for sql query

--- src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/RentalRepository.java
@@ -6,11 +6,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     Optional<Rental>findByRentalAddress(String address);
-    
+
+    @Transactional
+    @Modifying
+    @Query("update Rental r " +
+            "set r.avgRentAmount = ?2, r.totalRentIncome = ?3, r.totalUnits = ?4 " +
+            "where r.rentalAddress = ?1 ")
+    void updateRentalOnNewUnit(String rentalAddress, double newAvgRent, double newRentalRent, int newRentalAmount);
+
+    @Query("select r from Rental r")
+    Optional<List<Rental>> getAllUnits();
+
 }
