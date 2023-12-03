4a6f2d1a58a0ff6a295c736a87b1502e652543aa
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-02 23:29:42 -0500

    Added: model for getUnitsRequest

--- src/main/java/com/example/rentalmanagerapp/Rental/Units/Requests/ReturnGetUnitsRequest.java
+++ src/main/java/com/example/rentalmanagerapp/Rental/Units/Requests/ReturnGetUnitsRequest.java
@@ -1,2 +1,26 @@
-package com.example.rentalmanagerapp.Rental.Units.Requests;public class ReturnGetUnitsRequest {
+package com.example.rentalmanagerapp.Rental.Units.Requests;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReturnGetUnitsRequest {
+    private String unitAddress;
+    private int beds;
+    private double baths;
+    private int unitNumber;
+    private Boolean hasPets;
+    private double rentAmount;
+    private double rentDue;
+    private double rentPaid;
+    private LocalDate leaseStart;
+    private LocalDate rentDueDate;
+    private LocalDate leaseEnd;
+    
 }
