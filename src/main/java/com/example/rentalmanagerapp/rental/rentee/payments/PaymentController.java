a4ab72260492402ed715dd84f6d48f410e7480e0
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-03 22:52:57 -0500

    Added: boilerplate for payments controller

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentController.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentController.java
@@ -1,2 +1,14 @@
-package com.example.rentalmanagerapp.rental.rentee.payments;public class PaymentController {
+package com.example.rentalmanagerapp.rental.rentee.payments;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/payments")
+@AllArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
 }
