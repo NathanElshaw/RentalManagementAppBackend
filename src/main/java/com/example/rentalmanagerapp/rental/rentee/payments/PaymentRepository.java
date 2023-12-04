15e7decfe9bf177d63a4ba1441b346944e440f1f
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-03 22:53:32 -0500

    Added: boilerplate for payments repository

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentRepository.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentRepository.java
@@ -1,2 +1,8 @@
-package com.example.rentalmanagerapp.rental.rentee.payments;public interface PaymentRepository {
+package com.example.rentalmanagerapp.rental.rentee.payments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
