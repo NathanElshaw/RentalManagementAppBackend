c8e753a40313c41e8d1ee086a9cdf8a2324173d4
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-03 22:54:09 -0500

    Added: boilerplate for payments service

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentService.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentService.java
@@ -1,2 +1,18 @@
-package com.example.rentalmanagerapp.rental.rentee.payments;public class PaymentService {
+package com.example.rentalmanagerapp.rental.rentee.payments;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    //Todo:
+    //Call this api after payment confirmed
+    //get how much was paid
+    //then get unit via user id
+    // add payment amount to unit and subtract amount owed from payment
+    // add amount paid to rental total income 
 }
