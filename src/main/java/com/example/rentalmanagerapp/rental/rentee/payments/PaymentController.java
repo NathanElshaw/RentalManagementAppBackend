e47285abc255dad995d5554316a5bcc16afd2bac
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-04 22:46:43 -0500

    Added: createPayment method

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentController.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/payments/PaymentController.java
@@ -1,6 +1,8 @@
 package com.example.rentalmanagerapp.rental.rentee.payments;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentService paymentService;
+
+    @PostMapping("/create")
+    public String createPayment(
+            @RequestBody Payment.UserPaymentRequest paymentPayload){
+        return paymentService.createPayment(paymentPayload);
+    }
 
 }
