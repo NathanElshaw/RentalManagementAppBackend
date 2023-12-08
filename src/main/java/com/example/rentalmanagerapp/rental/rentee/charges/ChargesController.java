891c14afaa564c81df1de7823e246fdf4def5119
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-07 21:46:48 -0500

    Added: createCharge request

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesController.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesController.java
@@ -1,4 +1,23 @@
 package com.example.rentalmanagerapp.rental.rentee.charges;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/charges")
+@AllArgsConstructor
 public class ChargesController {
+
+    private final ChargesService chargesService;
+
+
+    @PostMapping("/create")
+    public String createCharge(
+            @RequestBody Charges.createChargeRequest chargesPayload){
+        return chargesService.createCharge(chargesPayload);
+    }
+
 }
