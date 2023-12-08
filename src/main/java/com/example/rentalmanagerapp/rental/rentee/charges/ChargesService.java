437b2e8ecdac9ee5f25fba5b5b6b3a0c6be03c97
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-07 21:50:31 -0500

    Added: createCharge service method

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesService.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesService.java
@@ -1,4 +1,17 @@
 package com.example.rentalmanagerapp.rental.rentee.charges;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ChargesService {
+
+    private final ChargesRepository chargesRepository;
+
+    public String createCharge(
+            Charges.createChargeRequest chargePayload){
+        return "";
+    }
+
 }
