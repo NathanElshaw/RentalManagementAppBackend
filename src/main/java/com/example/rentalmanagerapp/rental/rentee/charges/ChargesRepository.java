a5a5c0f5dafa794e393845fee6a0fa6f7e3fb4c7
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-08 23:07:51 -0500

    Added: findByChargeId and makePayment method

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesRepository.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesRepository.java
@@ -1,6 +1,34 @@
 package com.example.rentalmanagerapp.rental.rentee.charges;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
 public interface ChargesRepository extends JpaRepository<Charges, Long> {
+
+    @Query("select c from Charges c where c.chargeId = ?1 ")
+    Optional<Charges> findByChargeId(String chargeId);
+
+    @Modifying
+    @Transactional
+    @Query("update Charges c " +
+            "set c.amountPaid = ?2," +
+            "c.amountOwed = ?3, " +
+            "c.hasPaid = ?4," +
+            "c.paidAt =  ?5," +
+            "c.updatedAt = ?5 " +
+            "where c.chargeId = ?1" )
+    void makePayment(
+            String chargeId,
+            double paymentAmount,
+            double amountOwed,
+            boolean fullPayment,
+            LocalDateTime paidAt);
+
 }
