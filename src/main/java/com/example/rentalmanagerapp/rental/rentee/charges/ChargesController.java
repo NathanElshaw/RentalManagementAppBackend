650edcbe12557807d3456d349849b390b8b83a10
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-09 23:04:27 -0500

    Added: delete request

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesController.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesController.java
@@ -1,6 +1,7 @@
 package com.example.rentalmanagerapp.rental.rentee.charges;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,12 @@ public class ChargesController {
     public String createCharge(
             @RequestBody Charges.createChargeRequest chargesPayload){
         return chargesService.createCharge(chargesPayload);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteCharge(
+            @RequestBody Long chargeId) {
+        return chargesService.deleteCharge(chargeId);
     }
 
 }
