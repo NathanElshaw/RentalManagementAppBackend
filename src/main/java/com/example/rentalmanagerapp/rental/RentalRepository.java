6746f7f2381448d6d711c4f60d495fe5d407644b
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-11 23:10:46 -0500

    Added: queries for managers

--- src/main/java/com/example/rentalmanagerapp/rental/RentalRepository.java
+++ src/main/java/com/example/rentalmanagerapp/rental/RentalRepository.java
@@ -1,5 +1,6 @@
 package com.example.rentalmanagerapp.rental;
 
+import com.example.rentalmanagerapp.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -33,6 +34,23 @@ public interface RentalRepository extends JpaRepository<Rental, Long> {
             "set r.totalRentIncome = ?2 " +
             "where r.id = ?1 ")
     void updateRentalIncome(Long id, double newIncome);
+
+    @Transactional
+    @Modifying
+    @Query("update  Rental  r " +
+            "set r.assignedManager = ?2 " +
+            "where r.id = ?1")
+    void addManagerToRental(Long id, User manager);
+
+    @Transactional
+    @Modifying
+    @Query("update Rental r " +
+            "set r.assignedManager = ?2 " +
+            "where r.rentalAddress = ?1")
+    void addManagerToComplex(String address, User manager);
+
+    @Query("select r from Rental r where r.assignedManager = ?1 ")
+    Optional<List<Rental>> getRentalByAssignedManager(User user);
 
     @Query("select r from Rental r")
     Optional<List<Rental>> getAllUnits();
