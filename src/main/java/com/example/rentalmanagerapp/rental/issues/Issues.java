db111528929159a9f8ff6adcc086515b3f3770ee
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-15 23:41:27 -0500

    Added: start of db model

--- src/main/java/com/example/rentalmanagerapp/rental/issues/Issues.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/Issues.java
@@ -1,10 +1,17 @@
 package com.example.rentalmanagerapp.rental.issues;
 
+import com.example.rentalmanagerapp.rental.issues.enums.IssuePriority;
+import com.example.rentalmanagerapp.rental.issues.enums.IssueStatus;
+import com.example.rentalmanagerapp.user.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
@@ -12,4 +19,24 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Issues {
+
+    @Id
+    private Long id;
+
+    @OneToOne
+    private User createdBy;
+
+    private IssuePriority issuePriority;
+
+    private IssueStatus issueStatus;
+
+    @OneToOne
+    private User seenBy;
+
+    private String issueBody;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
 }
---
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/enums/IssueStatus.java
@@ -0,0 +1,2 @@
+package com.example.rentalmanagerapp.rental.issues.enums;public enum IssueStatus {
+}
