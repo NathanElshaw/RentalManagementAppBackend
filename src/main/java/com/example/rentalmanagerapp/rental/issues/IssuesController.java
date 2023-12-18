3f3fe3d8632c73711ba07c97f7017a5554ecebf7
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-17 19:37:10 -0500

    Added: createIssue method

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesController.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesController.java
@@ -13,5 +13,23 @@ package com.example.rentalmanagerapp.rental.issues;
 * getIssues from rental by status
 * getIssues for manager of rental
 */
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/issues")
 public class IssuesController {
+
+    private final IssuesServices issuesServices;
+
+    @PostMapping("/create")
+    public String createIssue(@RequestBody Issues.createRequest issuesPayload){
+        return issuesServices.createIssue(issuesPayload);
+    }
+
 }
