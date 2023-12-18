399f889f43f4f0e7436ecb21e2a0fd01af92f25c
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-17 19:37:31 -0500

    Added: createIsse method

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesServices.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesServices.java
@@ -1,4 +1,19 @@
 package com.example.rentalmanagerapp.rental.issues;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class IssuesServices {
+
+    public String createIssue(
+            Issues.createRequest issuePayload){
+        Issues newIssue = new Issues(
+                issuePayload.getUser(),
+                issuePayload.getUser().getU
+                issuePayload.getIssueBody(),
+                issuePayload.getPriority()
+        )
+        return "";
+    }
+
 }
