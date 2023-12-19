12b0315e00e250d7584c644848879c5a57f9e9fa
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-18 23:46:57 -0500

    Added: insertion to db

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesServices.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesServices.java
@@ -1,18 +1,40 @@
 package com.example.rentalmanagerapp.rental.issues;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class IssuesServices {
+
+    private final IssuesRepository repository;
+
+    public List<Issues> checkForIssue(Long userId){
+        return repository.checkForIssue(userId)
+                .orElseThrow(
+                        ()->new IllegalStateException("No Issues Found"));
+    }
 
     public String createIssue(
             Issues.createRequest issuePayload){
+        if(checkForIssue(
+                issuePayload.getUser().getId()).size() > 5){
+            throw  new IllegalStateException(
+                    "You have too many request open"
+            );
+        }
+
         Issues newIssue = new Issues(
                 issuePayload.getUser(),
-                issuePayload.getUser().getU
-                issuePayload.getIssueBody(),
-                issuePayload.getPriority()
-        )
+                issuePayload.getUser().getUsersUnit(),
+                issuePayload.getPriority(),
+                issuePayload.getIssueBody()
+        );
+
+        repository.save(newIssue);
+
         return "";
     }
 
