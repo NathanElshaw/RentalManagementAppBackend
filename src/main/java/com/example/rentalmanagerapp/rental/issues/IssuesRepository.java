41d9eec0ff9844d9cbbccfc50240836507a17dcf
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-18 23:44:36 -0500

    Added: checkForIssues repository method

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesRepository.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesRepository.java
@@ -1,8 +1,16 @@
 package com.example.rentalmanagerapp.rental.issues;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IssuesRepository  extends JpaRepository<Issues, Long> {
+    @Query("select i from Issues i where i.createdBy = ?1 ")
+    Optional<List<Issues>> checkForIssue (Long userId);
+
+
 }
