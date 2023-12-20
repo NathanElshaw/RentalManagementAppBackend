6f72ee816f2c7dc475325e9d2b639cffbf885b59
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-19 22:38:18 -0500

    Added: getIssuesForRental for manager method for service

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesServices.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesServices.java
@@ -1,8 +1,10 @@
 package com.example.rentalmanagerapp.rental.issues;
 
+import com.example.rentalmanagerapp.rental.units.Units;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -29,13 +31,41 @@ public class IssuesServices {
         Issues newIssue = new Issues(
                 issuePayload.getUser(),
                 issuePayload.getUser().getUsersUnit(),
+                issuePayload.getUser().getUsersUnit().getUnitNumber(),
                 issuePayload.getPriority(),
-                issuePayload.getIssueBody()
+                issuePayload.getIssueBody(),
+                issuePayload.getUser().getRentalAddress()
         );
 
         repository.save(newIssue);
 
         return "";
+    }
+
+    public List<Issues> getRentalIssues(String rentalAddress){
+        List<Issues> returnList = new ArrayList<>();
+
+        List<Issues> rentalsIssues = repository
+                .getRentalsIssuesByAddress(rentalAddress)
+                .orElseThrow(()-> new IllegalStateException("No Issues exist under than rental"));
+
+
+        rentalsIssues.forEach(
+                issues ->{
+                    Issues newListItem = new Issues(
+                        issues.getCreatedBy(),
+                        issues.getUnitNumber(),
+                        issues.getIssuePriority(),
+                        issues.getIssueBody(),
+                        issues.getIssueStatus(),
+                        issues.getAdminNotes()
+                );
+
+                returnList.add(newListItem);
+                }
+        );
+
+        return returnList;
     }
 
 }
