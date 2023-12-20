d4988241c258395e80b165b5a2ebcdaed981de79
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-19 22:37:32 -0500

    Added: getIssues for manager method

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesController.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesController.java
@@ -15,10 +15,9 @@ package com.example.rentalmanagerapp.rental.issues;
 */
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -30,6 +29,11 @@ public class IssuesController {
     @PostMapping("/create")
     public String createIssue(@RequestBody Issues.createRequest issuesPayload){
         return issuesServices.createIssue(issuesPayload);
+    }
+
+    @GetMapping("/manger/getRentalIssues")
+    public List<Issues> getRentalIssuesAsManager(String rentalAddress){
+        return issuesServices.getRentalIssues(rentalAddress);
     }
 
 }
