14b2f4fcdb673b76fa2cf8202ea8c15c14a3423c
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-26 23:24:00 -0500

    Added: tests for find by email and username, removed login test uneeded

--- src/test/java/com/example/rentalmanagerapp/user/UserRepositoryTest.java
+++ src/test/java/com/example/rentalmanagerapp/user/UserRepositoryTest.java
@@ -1,14 +1,10 @@
 package com.example.rentalmanagerapp.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Setter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.stereotype.Service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class UserRepositoryTest {
@@ -16,12 +12,19 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository underTest;
 
+
+    private final String name = "test";
+
+    private final String email = "Test@Test.com";
+
+    private final String username = "TestingUser";
+
     @Test
     void findByEmail() {
-        String email = "Test1@Test.com";
         User user = new User(
-                "Test1",
-                email
+                name,
+                email,
+                username
         );
         underTest.save(user);
 
@@ -30,15 +33,29 @@ class UserRepositoryTest {
                 .orElseThrow(()->new IllegalStateException(""));
 
         assertThat(testUser).isNotNull();
+        assertThat(testUser.getEmail()).isEqualTo(email);
+        assertThat(testUser.getUsername()).isEqualTo(username);
     }
 
     @Test
     void findByUsername() {
+
+        User user = new User(
+                name,
+                email,
+                username
+        );
+
+        underTest.save(user);
+
+        User testUser = underTest
+                .findByEmail(email)
+                .orElseThrow(()->new IllegalStateException(""));
+
+        assertThat(testUser).isNotNull();
+        assertThat(testUser.getUsername()).isEqualTo(username);
     }
 
-    @Test
-    void findByUsernameLogin() {
-    }
 
     @Test
     void addUnitToUser() {
