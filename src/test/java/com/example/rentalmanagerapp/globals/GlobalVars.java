3623931e4c7c645cb3662347a5d5291f3f816b18
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-27 21:58:15 -0500

    Added: global variables to use in tests

---
+++ src/test/java/com/example/rentalmanagerapp/globals/GlobalVars.java
@@ -0,0 +1,15 @@
+package com.example.rentalmanagerapp.globals;
+
+public interface GlobalVars {
+    public String name = "Test 1";
+
+    public String email = "Test1@test.com";
+
+    public String username = "TestingAccount";
+
+    public String address = "1234 testing ln";
+
+    public int unitNumber = 34;
+
+    public String rentalType = "Apartment";
+}
