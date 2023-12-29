6d7cafd08e71fc042eaf66962741fad917d55572
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-28 21:43:06 -0500

    Added: test

---
+++ src/test/java/com/example/rentalmanagerapp/rental/RentalRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.rentalmanagerapp.rental;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentalRepositoryTest {
+
+    @Test
+    void findByRentalAddress() {
+    }
+
+    @Test
+    void updateRentalOnNewUnit() {
+    }
+
+    @Test
+    void updateRentalIncome() {
+    }
+
+    @Test
+    void updateRental() {
+    }
+
+    @Test
+    void addManagerToRental() {
+    }
+
+    @Test
+    void addManagerToComplex() {
+    }
+
+    @Test
+    void getRentalByAssignedManager() {
+    }
+
+    @Test
+    void getAllUnits() {
+    }
+}
