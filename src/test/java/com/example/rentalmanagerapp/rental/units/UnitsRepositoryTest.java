b0d2d5e898e70cd071e76f81f81dc9248c9aff40
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-28 21:44:35 -0500

    Added: test for units

--- src/test/java/com/example/rentalmanagerapp/rental/units/UnitsRepositoryTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/units/UnitsRepositoryTest.java
@@ -1,4 +1,56 @@
+package com.example.rentalmanagerapp.rental.units;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UnitsRepositoryTest {
-  
+
+    @Test
+    void getUnitByUserId() {
+    }
+
+    @Test
+    void getUnitCodeParent() {
+    }
+
+    @Test
+    void findByAddressAndUnitNumber() {
+    }
+
+    @Test
+    void findByUser() {
+    }
+
+    @Test
+    void getUnitsByParentId() {
+    }
+
+    @Test
+    void addUnitCodeToRental() {
+    }
+
+    @Test
+    void addRenterToUnit() {
+    }
+
+    @Test
+    void findByUnitAddressAndUnitNumber() {
+    }
+
+    @Test
+    void updateUnit() {
+    }
+
+    @Test
+    void userPayment() {
+    }
+
+    @Test
+    void getAllUnitByAddress() {
+    }
+
+    @Test
+    void getAllUnits() {
+    }
 }
