c1763dcd93fe6a9d3f9589fb0eb276a46902248c
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-28 21:44:12 -0500

    Added: test for unitcodes

--- src/test/java/com/example/rentalmanagerapp/rental/unitcode/UnitCodeRepositoryTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/unitcode/UnitCodeRepositoryTest.java
@@ -1,4 +1,20 @@
+package com.example.rentalmanagerapp.rental.unitcode;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UnitCodeRepositoryTest {
-  
+
+    @Test
+    void findByUnitCode() {
+    }
+
+    @Test
+    void updateConfirmedAt() {
+    }
+
+    @Test
+    void getUnitWithCode() {
+    }
 }
