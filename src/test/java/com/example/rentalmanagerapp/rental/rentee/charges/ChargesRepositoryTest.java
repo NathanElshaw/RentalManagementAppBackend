6113062430ba779a46da69f7e5756ebf51a8f979
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-12-28 21:43:49 -0500

    Added: test for charges repository

--- src/test/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesRepositoryTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesRepositoryTest.java
@@ -1,4 +1,16 @@
+package com.example.rentalmanagerapp.rental.rentee.charges;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ChargesRepositoryTest {
-  
+
+    @Test
+    void findByChargeId() {
+    }
+
+    @Test
+    void makePayment() {
+    }
 }
