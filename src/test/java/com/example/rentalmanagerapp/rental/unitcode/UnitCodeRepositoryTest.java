e8ade8fa4e95ed971c4f1f72bfd03b25668dbd1b
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-03 23:42:28 -0500

    Added: test for findById

--- src/test/java/com/example/rentalmanagerapp/rental/unitcode/UnitCodeRepositoryTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/unitcode/UnitCodeRepositoryTest.java
@@ -1,16 +1,56 @@
 package com.example.rentalmanagerapp.rental.unitcode;
 
+import com.example.rentalmanagerapp.rental.Rental;
+import com.example.rentalmanagerapp.rental.RentalRepository;
+import com.example.rentalmanagerapp.rental.units.Units;
+import com.example.rentalmanagerapp.user.User;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static com.example.rentalmanagerapp.globals.GlobalVars.address;
+import static com.example.rentalmanagerapp.globals.GlobalVars.email;
+import static com.example.rentalmanagerapp.globals.GlobalVars.name;
+import static com.example.rentalmanagerapp.globals.GlobalVars.rentalType;
+import static com.example.rentalmanagerapp.globals.GlobalVars.unitNumber;
+import static com.example.rentalmanagerapp.globals.GlobalVars.username;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class UnitCodeRepositoryTest {
 
+    @Autowired
+    private RentalRepository rentalRepository;
+
+    void spinUp(){
+
+        Rental parentRental = rentalRepository
+                .findByRentalAddress(address)
+                .orElseThrow(()-> new IllegalStateException(""));
+
+        assertThat(parentRental).isNotNull();
+        assertThat(parentRental.getId()).isEqualTo(1L);
+    }
+
     @Test
     void findByUnitCode() {
+        rentalRepository.save(
+                new Rental(
+                        address,
+                        rentalType
+                )
+        );
+
+        Rental testRental = rentalRepository
+                .findById(1L)
+                .orElseThrow(()->new IllegalStateException(""));
+
+        assertThat(testRental).isNotNull();
+        assertThat(testRental.getId()).isEqualTo(1L);
+        assertThat(testRental.getRentalAddress()).isEqualTo(address);
+        assertThat(testRental.getType()).isEqualTo(rentalType);
     }
 
     @Test
