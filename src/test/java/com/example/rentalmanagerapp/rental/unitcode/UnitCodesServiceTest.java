c990cc33e6b234fba447e2716ed5661c1f882dd1
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-05 23:15:04 -0500

    Added: start of  joinUnit test

--- src/test/java/com/example/rentalmanagerapp/rental/unitcode/UnitCodesServiceTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/unitcode/UnitCodesServiceTest.java
@@ -4,6 +4,7 @@ import com.example.rentalmanagerapp.rental.Rental;
 import com.example.rentalmanagerapp.rental.RentalRepository;
 import com.example.rentalmanagerapp.rental.units.Units;
 import com.example.rentalmanagerapp.rental.units.UnitsRepository;
+import com.example.rentalmanagerapp.user.User;
 import com.example.rentalmanagerapp.user.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,11 +13,16 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import static com.example.rentalmanagerapp.globals.GlobalVars.address;
+import static com.example.rentalmanagerapp.globals.GlobalVars.email;
+import static com.example.rentalmanagerapp.globals.GlobalVars.name;
 import static com.example.rentalmanagerapp.globals.GlobalVars.rentalType;
 import static com.example.rentalmanagerapp.globals.GlobalVars.unitNumber;
+import static com.example.rentalmanagerapp.globals.GlobalVars.username;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
@@ -99,6 +105,12 @@ class UnitCodesServiceTest {
 
         String code = UUID.randomUUID().toString();
 
+        User user = new User(
+                name,
+                email,
+                username
+        );
+
         Rental rental = new Rental(
                 address,
                 rentalType
@@ -110,18 +122,80 @@ class UnitCodesServiceTest {
                 rental
         );
 
+
         UnitCodes testCode = new UnitCodes(
                 code,
                 unit
         );
 
+        unitCodeRepository.save(testCode);
+
+
+        given(unitCodeRepository
+                .findByUnitCode(code)).willReturn(Optional.of(testCode));
+
         given(unitsRepository
-                .assertUnitExistsById(unit.
-                        getId())).willReturn(true);
+                .findById(testCode
+                        .getParentRental()
+                        .getId()))
+                .willReturn(
+                        Optional.of(unit));
+
+        given(userRepository
+                .findById(
+                        unit.getId()))
+                .willReturn(Optional.of(user));
 
         underTest.createUnitCode(testCode);
 
+        ArgumentCaptor<String> stringArgumentCaptor =
+                ArgumentCaptor.forClass(String.class);
 
+        ArgumentCaptor<Long> longArgumentCaptor =
+                ArgumentCaptor.forClass(Long.class);
+
+        ArgumentCaptor<Integer> intArgumentCaptor =
+                ArgumentCaptor.forClass(Integer.class);
+
+        ArgumentCaptor<LocalDateTime> dateTimeArgumentCaptor =
+                ArgumentCaptor.forClass(LocalDateTime.class);
+
+        ArgumentCaptor<User> userArgumentCaptor =
+                ArgumentCaptor.forClass(User.class);
+
+        ArgumentCaptor<UnitCodes> unitCodesArgumentCaptor =
+                ArgumentCaptor.forClass(UnitCodes.class);
+
+        ArgumentCaptor<Units> unitsArgumentCaptor =
+                ArgumentCaptor.forClass(Units.class);
+
+        verify(userRepository)
+                .findById(longArgumentCaptor.capture());
+
+        verify(unitsRepository)
+                .findById(longArgumentCaptor.capture());
+
+        verify(unitCodeRepository)
+                .findByUnitCode(stringArgumentCaptor.capture());
+
+        verify(unitCodeRepository)
+                .updateConfirmedAt(
+                        stringArgumentCaptor.capture(),
+                        dateTimeArgumentCaptor.capture()
+                );
+
+        verify(userRepository)
+                .addUnitToUser(
+                        userArgumentCaptor.capture(),
+                        unitsArgumentCaptor.capture()
+                );
+
+        verify(unitsRepository)
+                .addUnitCodeToRental(
+                        unitCodesArgumentCaptor.capture(),
+                        stringArgumentCaptor.capture(),
+                        intArgumentCaptor.capture()
+                );
     }
 
     @Test
