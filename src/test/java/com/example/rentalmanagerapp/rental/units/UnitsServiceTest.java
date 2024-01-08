d0bce53a5158a033f8b8790c9393fdc93f5dd9e4
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-07 22:30:55 -0500

    Added: boiler plate of tests

--- src/test/java/com/example/rentalmanagerapp/rental/units/UnitsServiceTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/units/UnitsServiceTest.java
@@ -1,4 +1,32 @@
+package com.example.rentalmanagerapp.rental.units;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UnitsServiceTest {
-  
+
+    @Test
+    void createUnit() {
+    }
+
+    @Test
+    void getAllUnitsByAddress() {
+    }
+
+    @Test
+    void getRentalWithCode() {
+    }
+
+    @Test
+    void updateUnit() {
+    }
+
+    @Test
+    void userIdGetUnits() {
+    }
+
+    @Test
+    void getAllUnitsWithDetails() {
+    }
 }
