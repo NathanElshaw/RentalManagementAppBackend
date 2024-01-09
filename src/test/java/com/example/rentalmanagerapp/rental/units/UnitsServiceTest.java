d44914174a9d2fab9271fa779e615dd459510c48
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-08 22:02:35 -0500

    Added: tests

--- src/test/java/com/example/rentalmanagerapp/rental/units/UnitsServiceTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/units/UnitsServiceTest.java
@@ -1,17 +1,145 @@
 package com.example.rentalmanagerapp.rental.units;
 
+import com.example.rentalmanagerapp.rental.Rental;
+import com.example.rentalmanagerapp.rental.RentalRepository;
+import com.example.rentalmanagerapp.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.rentalmanagerapp.globals.GlobalVars.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UnitsServiceTest {
 
+    @Mock
+    private UnitsRepository unitsRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RentalRepository rentalRepository;
+
+    @InjectMocks
+    private UnitsService underTest;
+
+    private Units unit;
+
+    private Rental rental;
+
+    @BeforeEach
+     void spinUp(){
+        underTest = new UnitsService(
+                unitsRepository,
+                userRepository,
+                rentalRepository
+        );
+
+        rental = new Rental(
+            address,
+            rentalType
+        );
+
+        unit = new Units(
+                address,
+                unitNumber,
+                rental
+        );
+
+    }
+
     @Test
-    void createUnit() {
+    void canCreateUnit() {
+
+        when(unitsRepository
+                .assertUnitExistByAddressAndNumber(
+                        unit.getUnitAddress(),
+                        unit.getUnitNumber()
+                )).thenReturn(false);
+
+        when(rentalRepository
+                .findByRentalAddress(address))
+                .thenReturn(Optional.of(rental));
+
+        underTest.createUnit(unit);
+
+        ArgumentCaptor<Units> unitsArgumentCaptor =
+                ArgumentCaptor.forClass(Units.class);
+
+        verify(unitsRepository)
+                .save(unitsArgumentCaptor.capture());
+
+        Units testUnit = unitsArgumentCaptor.getValue();
+
+        assertThat(testUnit).isNotNull();
+        assertThat(testUnit.getUnitAddress()).isEqualTo(address);
+    }
+
+    @Test
+    void createUnitWillThrowUnitExists(){
+        when(unitsRepository
+                .assertUnitExistByAddressAndNumber(
+                        unit.getUnitAddress(),
+                        unit.getUnitNumber()
+                )).thenReturn(true);
+
+        assertThatThrownBy(()->underTest.createUnit(unit))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Unit already exists");
+    }
+
+    @Test
+    void createUnitWillThrowNoParent(){
+        when(unitsRepository
+                .assertUnitExistByAddressAndNumber(
+                        unit.getUnitAddress(),
+                        unit.getUnitNumber()
+                )).thenReturn(false);
+
+        assertThatThrownBy(()->underTest.createUnit(unit))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Parent Rental doesnt exist");
     }
 
     @Test
     void getAllUnitsByAddress() {
+
+        List<Units> unitsList = new ArrayList<>();
+
+        unitsList.add(unit);
+
+        when(unitsRepository
+                .getAllUnitByAddress(address))
+                .thenReturn(unitsList);
+
+        underTest.getAllUnitsByAddress(address);
+
+        ArgumentCaptor<String> stringArgumentCaptor =
+                ArgumentCaptor.forClass(String.class);
+
+        verify(unitsRepository)
+                .getAllUnitByAddress(stringArgumentCaptor.capture());
+
+        List<Units> testUnits = underTest.getAllUnitsByAddress(address);
+
+        assertThat(testUnits).isNotNull();
+        assertThat(testUnits.size()).isEqualTo(1);
+        assertThat(testUnits.get(0).getUnitNumber()).isEqualTo(unitNumber);
     }
 
     @Test
@@ -20,6 +148,26 @@ class UnitsServiceTest {
 
     @Test
     void updateUnit() {
+        when(unitsRepository.findById(unit.getId()))
+                .thenReturn(Optional.of(unit));
+
+        underTest.updateUnit(unit);
+
+        ArgumentCaptor<Long> longArgumentCaptor =
+                ArgumentCaptor.forClass(Long.class);
+
+        ArgumentCaptor<Units> unitsArgumentCaptor =
+                ArgumentCaptor.forClass(Units.class);
+
+        verify(unitsRepository).updateUnit(
+                longArgumentCaptor.capture(),
+                unitsArgumentCaptor.capture()
+        );
+
+        Units testUnits = unitsArgumentCaptor.getValue();
+
+        assertThat(testUnits).isNotNull();
+        assertThat(testUnits.getUnitNumber()).isEqualTo(unitNumber);
     }
 
     @Test
