026b16746876c2fd84fce36edebf695ccdfa35cc
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-10 22:42:27 -0500

    Added: tests

--- src/test/java/com/example/rentalmanagerapp/rental/RentalRepositoryTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/RentalRepositoryTest.java
@@ -1,16 +1,54 @@
 package com.example.rentalmanagerapp.rental;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static com.example.rentalmanagerapp.globals.GlobalVars.address;
+import static com.example.rentalmanagerapp.globals.GlobalVars.rentalType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class RentalRepositoryTest {
 
+    @Autowired
+    private RentalRepository underTest;
+
+    @BeforeEach
+    void spinUp(){
+        underTest.save(
+                new Rental(
+                        address,
+                        rentalType
+                )
+        );
+    }
+
+    @AfterEach
+    void spinDown(){
+        underTest.deleteAll();
+    }
+
     @Test
     void findByRentalAddress() {
+        Rental testRental = underTest
+                .findByRentalAddress(address)
+                .orElseThrow(()-> new IllegalStateException("A"));
+
+        assertThat(testRental).isNotNull();
+        assertThat(testRental.getRentalAddress()).isEqualTo(address);
+    }
+
+    @Test
+    void getRentalByAssignedManager() {
+    }
+
+    @Test
+    void getAllUnits() {
     }
 
     @Test
@@ -36,13 +74,5 @@ class RentalRepositoryTest {
     @Test
     @Disabled
     void addManagerToComplex() {
-    }
-
-    @Test
-    void getRentalByAssignedManager() {
-    }
-
-    @Test
-    void getAllUnits() {
     }
 }
