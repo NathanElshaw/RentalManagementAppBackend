db1d069f7786b285146fcdcc352898e8ded0b159
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-12 23:43:51 -0500

    Added: tests for service class

--- src/test/java/com/example/rentalmanagerapp/rental/RentalServiceTest.java
+++ src/test/java/com/example/rentalmanagerapp/rental/RentalServiceTest.java
@@ -10,7 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static com.example.rentalmanagerapp.globals.GlobalVars.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -64,10 +68,55 @@ class RentalServiceTest {
 
         verify(rentalRepository)
                 .save(rentalArgumentCaptor.capture());
+
+        Rental testRental = rentalArgumentCaptor.getValue();
+
+        assertThat(testRental).isNotNull();
+        assertThat(testRental.getRentalAddress()).isEqualTo(address);
+    }
+
+    @Test
+    void createRentalWillThrow(){
+
+        when(rentalRepository
+                .assertRentalByAddress(address))
+                .thenReturn(true);
+
+        assertThatThrownBy(()->underTest.createRental(rental))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Address already exists");
     }
 
     @Test
     void updateRental() {
+
+        when(rentalRepository.findById(rental.getId()))
+                .thenReturn(Optional.of(rental));
+
+        underTest.updateRental(rental);
+
+        ArgumentCaptor<Rental>  rentalArgumentCaptor =
+                ArgumentCaptor.forClass(Rental.class);
+
+        ArgumentCaptor<Long> longArgumentCaptor =
+                ArgumentCaptor.forClass(Long.class);
+
+        verify(rentalRepository).updateRental(
+                longArgumentCaptor.capture(),
+                rentalArgumentCaptor.capture()
+        );
+
+        Rental testRental = rentalArgumentCaptor.getValue();
+
+        assertThat(testRental).isNotNull();
+        assertThat(testRental.getRentalAddress()).isEqualTo(address);
+    }
+
+    @Test
+    void updateRentalWillThrow(){
+        assertThatThrownBy(()->underTest.updateRental(rental))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("No Rentals Exist");
     }
 
     @Test
