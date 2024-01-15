6c3e6a5e225ac3d5639934b92d7b16aaa0a28a7a
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-14 23:20:14 -0500

    Boiler plate of User Service class tests

---
+++ src/test/java/com/example/rentalmanagerapp/user/UserServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.rentalmanagerapp.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void userLogin() {
+    }
+}
