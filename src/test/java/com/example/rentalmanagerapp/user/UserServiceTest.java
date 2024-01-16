52019f93da4935a2f3c8af1a659caa4d5a409b8f
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-15 22:34:47 -0500

    Added: tests

--- src/test/java/com/example/rentalmanagerapp/user/UserServiceTest.java
+++ src/test/java/com/example/rentalmanagerapp/user/UserServiceTest.java
@@ -1,20 +1,174 @@
 package com.example.rentalmanagerapp.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
+import static com.example.rentalmanagerapp.globals.GlobalVars.email;
+import static com.example.rentalmanagerapp.globals.GlobalVars.username;
+import static com.example.rentalmanagerapp.globals.GlobalVars.name;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder encoder;
+
+    @InjectMocks
+    private UserService underTest;
+
+    private User user;
+
+    private User.UserLoginRequest login = new User.UserLoginRequest(
+            username,
+            "Password"
+    );
+
+
+    @BeforeEach
+    void spinUp(){
+        underTest = new UserService(
+                userRepository,
+                encoder
+        );
+
+        user = new User(
+                name,
+                email,
+                username
+        );
+    }
 
     @Test
     void createUser() {
+
+        underTest.createUser(user);
+
+        ArgumentCaptor<User> userArgumentCaptor =
+                ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository).save(userArgumentCaptor.capture());
+
+        User testUser = userArgumentCaptor.getValue();
+
+        assertThat(testUser).isNotNull();
+        assertThat(testUser.getEmail()).isEqualTo(email);
+        assertThat(testUser.getUsername()).isEqualTo(username);
+    }
+
+    @Test
+    void createUserWillThrowEmail(){
+        when(userRepository.findByEmail(email))
+                .thenReturn(Optional.of(user));
+
+        assertThatThrownBy(()-> underTest.createUser(user))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Email already exists");
+    }
+
+    @Test
+    void createUserWillThrowUsername(){
+        when(userRepository.findByUsername(username))
+                .thenReturn(Optional.of(user));
+
+        assertThatThrownBy(()-> underTest.createUser(user))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("User already exists");
     }
 
     @Test
     void updateUser() {
+
+        when(userRepository.assertUserExists(user.getId()))
+                .thenReturn(true);
+
+        underTest.updateUser(user);
+
+        ArgumentCaptor<Long> longArgumentCaptor =
+                ArgumentCaptor.forClass(Long.class);
+
+        ArgumentCaptor<User> userArgumentCaptor =
+                ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository).updateUser(
+                longArgumentCaptor.capture(),
+                userArgumentCaptor.capture()
+        );
+    }
+
+    @Test
+    void updateUserWillThrow(){
+        assertThatThrownBy(()-> underTest.updateUser(user))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Cannot update");
+    }
+
+    @Test
+    void deleteUser(){
+
+        when(userRepository.assertUserExists(user.getId()))
+                .thenReturn(true);
+
+        underTest.deleteUser(user);
+
+        ArgumentCaptor<User> userArgumentCaptor =
+                ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository)
+                .delete(userArgumentCaptor.capture());
+    }
+
+    @Test
+    void deleteUserWillThrow(){
+        assertThatThrownBy(()-> underTest.deleteUser(user))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("User does not exist");
     }
 
     @Test
     void userLogin() {
+
+        CharSequence password = "password";
+
+        String encodedPassword = encoder.encode("password");
+
+        user.setPassword(encodedPassword);
+
+        when(userRepository.findByUsername(username))
+                .thenReturn(Optional.of(user));
+
+
+        when(encoder.matches(
+                password,
+                encodedPassword
+        )).thenReturn(true);
+
+        underTest.userLogin(login);
+
+        ArgumentCaptor<String> stringArgumentCaptor =
+                ArgumentCaptor.forClass(String.class);
+
+        verify(userRepository)
+                .findByUsername(stringArgumentCaptor.capture());
+
+        verify(encoder)
+                .matches(
+                        stringArgumentCaptor.capture(),
+                        stringArgumentCaptor.capture()
+                );
+
     }
 }
