a3afa039371f46880da94a1b08cf06664dc33ed0
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-18 21:53:07 -0500

    Added: locking down endpoints for auth

--- src/main/java/com/example/rentalmanagerapp/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/rentalmanagerapp/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.rentalmanagerapp.security.config;
 
+import com.example.rentalmanagerapp.user.UserRoles;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,10 +21,31 @@ public class WebSecurityConfig {
             HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/**",
+                    auth.requestMatchers(
                             "/error",
-                            "/api/v*/user/register/**").permitAll();
+                            "/api/v*/user/register/**")
+                            .permitAll();
                 })
+
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(
+                                    "/admin/**",
+                                    "/api/v1/**")
+                        .hasAuthority(String.valueOf(UserRoles.Admin));
+                })
+
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/v1/user",
+                                    "/api/v1/rentals/user/getRental",
+                                    "/api/v1/units/userIdGetRental",
+                                    "/api/v1/unitCodes/joinUnit",
+                                    "/api/v1/charges/user/**",
+                                    "/api/v1/issues/create",
+                                    "/api/v1/issues/update"
+                                    )
+                            .hasRole(String.valueOf(UserRoles.User));
+                })
+
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(withDefaults())
                 .build();
