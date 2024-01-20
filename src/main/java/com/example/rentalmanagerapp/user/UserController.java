c7d41c6763d035eb4c00bc36a5962d994a95efb0
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-19 23:33:13 -0500

    Changed: return type of loginUser

--- src/main/java/com/example/rentalmanagerapp/user/UserController.java
+++ src/main/java/com/example/rentalmanagerapp/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.rentalmanagerapp.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +38,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String loginUser(
+    public ResponseEntity<String> loginUser(
             @RequestBody User.UserLoginRequest loginPayload){
         return userService.userLogin(loginPayload);
     }
