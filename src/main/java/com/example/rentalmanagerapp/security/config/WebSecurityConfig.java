c07cbdcc22af4223671c89cbda71328a3beb92fc
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-20 19:38:10 -0500

    Added: start of jwt

--- src/main/java/com/example/rentalmanagerapp/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/rentalmanagerapp/security/config/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package com.example.rentalmanagerapp.security.config;
 
+import com.example.rentalmanagerapp.security.PasswordEncoder;
+import com.example.rentalmanagerapp.security.filter.JwtAuthFilter;
 import com.example.rentalmanagerapp.user.UserRoles;
+import com.example.rentalmanagerapp.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +21,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @AllArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    private final UserService userService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public authProvider authProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider =
+                new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(userService.userDetailService);
+
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder.bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception{
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(
---
+++ src/main/java/com/example/rentalmanagerapp/security/filter/JwtAuthFilter.java
@@ -0,0 +1,2 @@
+package com.example.rentalmanagerapp.security.filter;public class JwtAuthFilter {
+}
