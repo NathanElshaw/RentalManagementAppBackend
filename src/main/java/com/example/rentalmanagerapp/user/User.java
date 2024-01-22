13205abfe5590d0f4d8525b63da5fb4c15035568
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-21 23:07:14 -0500

    Added: userDetails to class

--- src/main/java/com/example/rentalmanagerapp/user/User.java
+++ src/main/java/com/example/rentalmanagerapp/user/User.java
@@ -13,9 +13,14 @@ import lombok.Setter;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.SEQUENCE;
 
@@ -24,7 +29,7 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 @Getter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @SequenceGenerator(
             name = "userSequence",
             sequenceName = "userSequence",
@@ -90,6 +95,37 @@ public class User {
     private LocalDateTime createdAt = LocalDateTime.now();
 
     private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        // our "username" for security is the email field
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     public User() {
 
