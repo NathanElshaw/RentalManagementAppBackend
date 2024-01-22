70807c5d73ea601c0b9249bc93f6c3e0057607d0
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-21 23:09:07 -0500

    Added: methods for jwtService

--- src/main/java/com/example/rentalmanagerapp/security/jwt/JwtService.java
+++ src/main/java/com/example/rentalmanagerapp/security/jwt/JwtService.java
@@ -1,2 +1,38 @@
-package com.example.rentalmanagerapp.security.jwt;public class JwtService {
+package com.example.rentalmanagerapp.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+
+@Service
+@AllArgsConstructor
+public class JwtService {
+
+    private static final String secretKey = "25fc6c89653b43b23bab963238e17c7fe4c260281e436db2e62df3dec109ab45";
+
+
+    public String extractUsername(String token){
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
