71d5fb5367015ebf260ac0cc4d741dbe401365be
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-26 21:27:28 -0500

    Added: permissions section

---
+++ src/main/java/com/example/rentalmanagerapp/user/Permissions.java
@@ -0,0 +1,35 @@
+package com.example.rentalmanagerapp.user;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Permissions {
+
+    Admin_Read("admin::read"),
+
+    Admin_Update("admin::update"),
+
+    Admin_Post("admin::post"),
+
+    Admin_Delete("admin::delete"),
+
+    Property_Manager_Read("property_manager::read"),
+
+    Property_Manager_Update("property_manager::update"),
+
+    Property_Manager_Post("property_manager::post"),
+
+    Property_Manager_Delete("property_manager::delete"),
+
+    User_Read("user::read"),
+
+    User_Update("user:update"),
+
+    User_Post("user::post"),
+
+    User_Delete("user_delete"),
+
+    Inactive_User_Post("inactive_user::post");
+
+    private final String permissions;
+}
