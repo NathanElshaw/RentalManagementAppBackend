76fdf79bf16204a5413508c5d89c6ac9d1d5b686
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-01-26 21:28:33 -0500

    Added: assigned permissions and added getAuthorities method

--- src/main/java/com/example/rentalmanagerapp/user/UserRoles.java
+++ src/main/java/com/example/rentalmanagerapp/user/UserRoles.java
@@ -1,8 +1,78 @@
 package com.example.rentalmanagerapp.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static com.example.rentalmanagerapp.user.Permissions.*;
+
+@RequiredArgsConstructor
 public enum UserRoles {
-    User,
-    Admin,
-    inActive_User,
-    PropertyManger
+
+    User(
+            Set.of(
+                    User_Read,
+                    User_Post,
+                    User_Update,
+                    User_Delete
+            )
+    ),
+
+    Admin(
+            Set.of(
+                    User_Read,
+                    User_Post,
+                    User_Update,
+                    User_Delete,
+                    Admin_Read,
+                    Admin_Post,
+                    Admin_Update,
+                    Admin_Delete,
+                    Property_Manager_Read,
+                    Property_Manager_Post,
+                    Property_Manager_Update,
+                    Property_Manager_Delete
+            )
+    ),
+
+    inActive_User(
+            Set.of(
+                    Inactive_User_Post
+            )
+    ),
+
+    PropertyManger(
+            Set.of(
+                    User_Read,
+                    User_Post,
+                    User_Update,
+                    User_Delete,
+                    Property_Manager_Read,
+                    Property_Manager_Post,
+                    Property_Manager_Update,
+                    Property_Manager_Delete
+            )
+    )
+
+    ;
+    @Getter
+    private final Set<Permissions> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities(){
+       List<SimpleGrantedAuthority> authorities =
+               new java.util.ArrayList<>(getPermissions()
+                       .stream()
+                       .map(permissions -> new SimpleGrantedAuthority(
+                               permissions.name()
+                       ))
+                       .toList());
+
+       authorities.add(new SimpleGrantedAuthority("ROlE_" + this.name()));
+
+       return authorities;
+    };
 }
