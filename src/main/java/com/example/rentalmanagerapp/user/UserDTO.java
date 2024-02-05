b89b36ae7eb2a6c2c8086943453d10afa05d85da
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-04 21:59:04 -0500

    Added: user dto for get requests

--- src/main/java/com/example/rentalmanagerapp/user/UserDTO.java
+++ src/main/java/com/example/rentalmanagerapp/user/UserDTO.java
@@ -1,4 +1,49 @@
 package com.example.rentalmanagerapp.user;
 
-public class UserDTO {
+import com.example.rentalmanagerapp.rental.units.Units;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record UserDTO (
+
+        Long id,
+
+        String name,
+
+        LocalDate birthDate,
+
+        String address,
+
+        String email,
+
+        String telephone,
+
+        String username,
+
+        UserRoles userRoles,
+
+        List<String> authorities,
+
+        LocalDate rentDue,
+
+        LocalDate rentLastPaid,
+
+        LocalDate dateLeaseStarted,
+
+        Long amountPaid,
+
+        Long amountOwed,
+
+        String rentalAddress,
+
+        Units usersUnit
+
+) {
+
 }
