67f28555fb0a67d01d0a2d015bb8e94eb1093dad
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-04 22:01:09 -0500

    Added: dto mapper for data transfer

--- src/main/java/com/example/rentalmanagerapp/user/UserDTOMapper.java
+++ src/main/java/com/example/rentalmanagerapp/user/UserDTOMapper.java
@@ -1,14 +1,37 @@
 package com.example.rentalmanagerapp.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Service
-@AllArgsConstructor
-public class UserDTOService {
+public class UserDTOMapper implements Function<User, UserDTO> {
 
-
-    public UserDTO convertUser(User user){
-
+    @Override
+    public UserDTO apply(User user){
+        return new UserDTO(
+                user.getId(),
+                user.getFullName(),
+                user.getBirthDate(),
+                user.getAddress(),
+                user.getEmail(),
+                user.getTelephone(),
+                user.getUsername(),
+                user.getUserRole(),
+                user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()),
+                user.getRentDue(),
+                user.getRentLastPaid(),
+                user.getDateLeaseStarted(),
+                user.getAmountPaid(),
+                user.getAmountOwed(),
+                user.getRentalAddress(),
+                user.getUsersUnit()
+        );
     }
 }
