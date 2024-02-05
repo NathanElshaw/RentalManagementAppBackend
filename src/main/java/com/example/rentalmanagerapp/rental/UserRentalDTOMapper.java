d3cacf202f4d436c5e26e250f78adaca4ecec44e
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-04 22:03:14 -0500

    Added: user rental dto mapper for user get requests

--- src/main/java/com/example/rentalmanagerapp/rental/UserRentalDTOMapper.java
+++ src/main/java/com/example/rentalmanagerapp/rental/UserRentalDTOMapper.java
@@ -1,2 +1,43 @@
+package com.example.rentalmanagerapp.rental;
 
+import com.example.rentalmanagerapp.rental.units.Units;
+import com.example.rentalmanagerapp.user.UserDTOMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+@AllArgsConstructor
+@Getter
+public class UserRentalDTOMapper implements Function<Rental.Dto, RentalDTO> {
+
+    private final UserDTOMapper dtoMapper;
+
+    @Override
+    public RentalDTO apply(Rental.Dto rentalDto){
+
+        Units unit = rentalDto.getUnits();
+
+        Rental rental = rentalDto.getRental();
+
+        return new RentalDTO(
+                rental.getRentalAddress(),
+                rental.getType(),
+                rental.getTotalUnits(),
+                dtoMapper.apply(
+                        rental.getAssignedManager()
+                ),
+                unit.getUnitNumber(),
+                unit.getBeds(),
+                unit.getBaths(),
+                unit.getRentAmount(),
+                unit.getLeaseStart(),
+                unit.getRentDueDate(),
+                unit.getLeaseEnd()
+        );
+    }
+}
 
