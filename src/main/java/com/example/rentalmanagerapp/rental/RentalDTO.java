f458d2a7359173cc106a405356847f9da3edc4c1
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-04 22:02:49 -0500

    Added: rental dto objects for user and admin

--- src/main/java/com/example/rentalmanagerapp/rental/RentalDTO.java
+++ src/main/java/com/example/rentalmanagerapp/rental/RentalDTO.java
@@ -1,2 +1,58 @@
-package com.example.rentalmanagerapp.rental;public record RentalDTO() {
+package com.example.rentalmanagerapp.rental;
+
+import com.example.rentalmanagerapp.user.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import javax.annotation.processing.Generated;
+import java.time.LocalDate;
+
+@Getter
+public record RentalDTO(
+        String address,
+
+        String type,
+
+        int totalUnits,
+
+        UserDTO assignedManager,
+
+        //Unit
+
+        int unitNumber,
+
+        int beds,
+
+        double baths,
+
+        double rentAmount,
+
+        LocalDate leaseStart,
+
+        LocalDate rentDueDate,
+
+        LocalDate leaseEnd
+) {
+    @Getter
+    @AllArgsConstructor
+    public record AdminRentalDTO(
+            String address,
+
+            String description,
+
+            String type,
+
+            int totalUnits,
+
+            int totalTenants,
+
+            double avgRentAmount,
+
+            double totalRentIncome,
+
+            UserDTO assignedManager
+    ){
+
+    }
 }
