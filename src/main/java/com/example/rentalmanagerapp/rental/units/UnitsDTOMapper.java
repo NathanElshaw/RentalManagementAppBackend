ffd2ace3903b876cc6e7547233793d3f090b0df3
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-05 21:40:39 -0500

    Added: units dto transfer object

--- src/main/java/com/example/rentalmanagerapp/rental/units/UnitsDTOMapper.java
+++ src/main/java/com/example/rentalmanagerapp/rental/units/UnitsDTOMapper.java
@@ -1,2 +1,22 @@
-package com.example.rentalmanagerapp.rental.units;public class UnitsDTOMapper {
+package com.example.rentalmanagerapp.rental.units;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class UnitsDTOMapper implements Function<Units, UnitsDTO> {
+
+    @Override
+    public UnitsDTO apply(Units unit){
+        return new UnitsDTO(
+                unit.getId(),
+                unit.getUnitAddress(),
+                unit.getBeds(),
+                unit.getBaths(),
+                unit.getUnitNumber()
+        );
+    }
+
+
 }
