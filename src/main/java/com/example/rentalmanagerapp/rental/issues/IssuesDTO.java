b64530e9cc4ced5f327173746036ed029c72ac1f
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-05 21:37:20 -0500

    Added: added issues dto

--- src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesDTO.java
+++ src/main/java/com/example/rentalmanagerapp/rental/issues/IssuesDTO.java
@@ -1,2 +1,21 @@
-package com.example.rentalmanagerapp.rental.issues;public class IssuesDTO {
+package com.example.rentalmanagerapp.rental.issues;
+
+import com.example.rentalmanagerapp.rental.issues.enums.IssuePriority;
+import com.example.rentalmanagerapp.rental.issues.enums.IssueStatus;
+import com.example.rentalmanagerapp.rental.units.UnitsDTO;
+import com.example.rentalmanagerapp.user.UserDTO;
+
+public record IssuesDTO (
+        UserDTO createBy,
+
+        UserDTO seenBy,
+
+        UnitsDTO unit,
+
+        IssuePriority issuePriority,
+
+        IssueStatus issueStatus,
+
+        String issueBody
+){
 }
