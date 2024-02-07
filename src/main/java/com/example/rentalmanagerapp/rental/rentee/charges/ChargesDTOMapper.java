13feab46060b190653ae903d2f0ba27dc3834898
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-06 21:44:47 -0500

    Added: dto mapper for charges

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesDTOMapper.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesDTOMapper.java
@@ -1,2 +1,34 @@
-package com.example.rentalmanagerapp.rental.rentee.charges;public class ChargesDTOMapper {
+package com.example.rentalmanagerapp.rental.rentee.charges;
+
+import com.example.rentalmanagerapp.user.UserDTOMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@AllArgsConstructor
+@Service
+public class ChargesDTOMapper implements Function<Charges, ChargesDTO> {
+
+    private final UserDTOMapper userDTOMapper;
+
+    @Override
+    public ChargesDTO apply(Charges charge){
+        return new ChargesDTO(
+                userDTOMapper.apply(
+                       charge.getUser()
+               ),
+                charge.getReason(),
+                userDTOMapper.apply(
+                       charge.getCreatedBy()
+               ),
+                charge.isHasPaid(),
+                charge.getAmountOwed(),
+                charge.getAmountPaid(),
+                charge.getPaidAt(),
+                charge.getDueBy(),
+                charge.getChargeId(),
+                charge.getTransactionId()
+        );
+    }
 }
