901fb917eee11c40f2bbdf9b3c6658af0720b322
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-06 21:44:33 -0500

    Added: charges dto

--- src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesDTO.java
+++ src/main/java/com/example/rentalmanagerapp/rental/rentee/charges/ChargesDTO.java
@@ -1,2 +1,29 @@
-package com.example.rentalmanagerapp.rental.rentee.charges;public interface ChargesDTO {
+package com.example.rentalmanagerapp.rental.rentee.charges;
+
+import com.example.rentalmanagerapp.user.UserDTO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record ChargesDTO (
+        UserDTO user,
+
+        String reason,
+
+        UserDTO createdBy,
+
+        boolean hasPaid,
+
+        double amountOwed,
+
+        double amountPaid,
+
+        LocalDateTime paidAt,
+
+        LocalDate dueBy,
+
+        String chargeId,
+
+        String transactionId
+){
 }
