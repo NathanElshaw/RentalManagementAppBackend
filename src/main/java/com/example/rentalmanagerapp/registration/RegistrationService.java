1d55be406c1508dac1faf525d13b301253a3c200
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-02-08 21:56:02 -0500

    Added: create code method

--- src/main/java/com/example/rentalmanagerapp/registration/RegistrationService.java
+++ src/main/java/com/example/rentalmanagerapp/registration/RegistrationService.java
@@ -4,7 +4,11 @@ import com.example.rentalmanagerapp.exceptions.BadRequestException;
 import com.example.rentalmanagerapp.user.User;
 import com.example.rentalmanagerapp.user.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -13,8 +17,26 @@ public class RegistrationService {
     private final UserRepository userRepository;
 
     public String createToken(){
+        Random random = new Random();
         //Todo: add code creator
-        return "";
+        String[] validChars = {
+                "a", "b", "c", "d", "e", "f", "g",
+                "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "t", "u", "v",
+                "w", "x", "y", "z", "1", "2", "3",
+                "4", "5", "6", "7", "8", "9", "0"
+        };
+
+        StringBuilder code = new StringBuilder();
+
+        for(int i = 0; i < 6; i++){
+            code
+                    .append(
+                            validChars[ random.ints(0, 35)
+                                    .findFirst()
+                                    .getAsInt()]);
+        }
+        return code.toString();
     }
 
     public String sendToken(
