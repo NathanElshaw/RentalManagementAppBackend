0f4eb60b9df3b98379ddb19ed3c6df53950b4488
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-08 23:59:44 -0400

    Added: review class for reputation class

--- src/main/java/com/example/rentalmanagerapp/user/reputation/Review.java
+++ src/main/java/com/example/rentalmanagerapp/user/reputation/Review.java
@@ -1,2 +1,51 @@
-package com.example.rentalmanagerapp.user.reputation;public class Review {
+package com.example.rentalmanagerapp.user.reputation;
+
+import com.example.rentalmanagerapp.user.User;
+import com.example.rentalmanagerapp.user.UserDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "UserReviews")
+public class Review {
+
+    @SequenceGenerator(
+            name = "reviewSequence",
+            sequenceName = "reviewSequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "reviewSequence"
+    )
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    private User reviewer;
+
+    private int reviewScore;
+
+    private String reviewBody;
+
+    private LocalDateTime reviewDateTime;
+
+    private LocalDateTime updatedAt;
+
+    public Review() {
+
+    }
+
 }
