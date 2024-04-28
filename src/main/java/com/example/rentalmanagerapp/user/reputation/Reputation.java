5a9f640137f0f45e0553c33a0b2a7876175b3863
Nathan Elshaw <121355645+NathanElshaw@users.noreply.github.com>
2024-04-27 23:27:20 -0400

    Changed into a data table for correct annotation for user model

--- src/main/java/com/example/rentalmanagerapp/user/reputation/Reputation.java
+++ src/main/java/com/example/rentalmanagerapp/user/reputation/Reputation.java
@@ -1,18 +1,40 @@
 package com.example.rentalmanagerapp.user.reputation;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "Reputation")
 public class Reputation {
+
+    @SequenceGenerator(
+            name = "userScoreSequence",
+            sequenceName = "userScoreSequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "userScoreSequence"
+    )
+
+    @Id
+    private Long id;
+
     private int repScore;
 
-    private Review[] usersReviews;
+    @ManyToOne
+    private Review usersReviews;
 
     private LocalDateTime updatedAt;
 
